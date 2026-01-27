9b20b196dc8f189ce8733e5d66ffe9ced8d2cbb3
Casa <pinedaarenasy@gmail.com>
2026-01-27 01:25:20 +0100

    adasdas

---
+++ PelisPostgres/src/main/java/org/example/controller/RestActores.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(RestActores.MAPPING)
+public class RestActores {
+
+
+    public static final String MAPPING = "postgres/RestActores";
+}
